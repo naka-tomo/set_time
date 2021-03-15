e774a38719fd7dbc697ba431564415d389b4faab
なかとも <20672947+naka-tomo@users.noreply.github.com>
2021-03-15 18:10:52 +0900

    Update set_datetime.py

--- set_datetime.py
+++ set_datetime.py
@@ -6,7 +6,5 @@ url = 'http://worldtimeapi.org/api/timezone/Asia/Tokyo'
 
 req = urllib.request.Request(url)
 text = urllib.request.urlopen(req).read().decode()
-print(text)
 
-print( json.loads(text) )
-os.system( "sudo date -s " + json.loads(text)["datetime"]  )
+print( json.loads(text)["datetime"]  )
